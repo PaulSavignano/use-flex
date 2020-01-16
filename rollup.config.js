9fc59b9eb1b68d9dcfa7ca72e31083ff3454b460
Paul Savignano <Paul.Savignano@gmail.com>
2020-01-15 16:22:01 -0800

    [package.json] add @rollup/plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss'
